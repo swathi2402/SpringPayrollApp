cf7ea3fea279282519aa0bd1334376b1861ca267
Swathi <swathi.h@perfios.com>
2021-11-01 22:31:19 +0530

    ADDED: Initializing project structure

---
+++ src/main/java/com/brigdelabz/springpayrollapp/SpringPayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.brigdelabz.springpayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayrollAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brigdelabz/springpayrollapp/SpringPayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigdelabz.springpayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
