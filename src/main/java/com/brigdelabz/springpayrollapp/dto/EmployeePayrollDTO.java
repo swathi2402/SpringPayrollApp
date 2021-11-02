537ca7f927de8939a49151cd8aa10e4c37f46ae2
Swathi <swathi.h@perfios.com>
2021-11-02 17:31:03 +0530

    ADDED: Methods to demonstarate the HTTP method calls

---
+++ src/main/java/com/brigdelabz/springpayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,18 @@
+package com.brigdelabz.springpayrollapp.dto;
+
+public class EmployeePayrollDTO {
+
+	public String name;
+	public long salary;
+
+	public EmployeePayrollDTO(String name, long salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "name=" + name + ", salary=" + salary;
+	}
+}
