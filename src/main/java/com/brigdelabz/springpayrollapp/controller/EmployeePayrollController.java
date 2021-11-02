61ee99d8335ffdb1c0d3acedf9f6c5b08f154a62
Swathi <swathi.h@perfios.com>
2021-11-02 17:40:14 +0530

    ADDED: DTO and model objects to ensure get call working

--- src/main/java/com/brigdelabz/springpayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/brigdelabz/springpayrollapp/controller/EmployeePayrollController.java
@@ -12,31 +12,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brigdelabz.springpayrollapp.dto.EmployeePayrollDTO;
+import com.brigdelabz.springpayrollapp.dto.ResponseDTO;
+import com.brigdelabz.springpayrollapp.model.EmployeePayrollData;
 
 @RestController
 @RequestMapping("/payrollservice")
 public class EmployeePayrollController {
 
 	@RequestMapping(value = { "", "/", "/get" })
-	public ResponseEntity<String> getEmployeePayrollData() {
-		return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
+	public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
+		EmployeePayrollData employeePayrollData = new EmployeePayrollData(1, new EmployeePayrollDTO("Lisa", 2000));
+		ResponseDTO responseDTO = new ResponseDTO("Get Call Success", employeePayrollData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
 	@GetMapping("/get/{empId}")
 	public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId) {
 		return new ResponseEntity<String>("Get Call Success for id: " + empId, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/create")
 	public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		return new ResponseEntity<String>("Created Employee Payroll Data for: " + employeePayrollDTO, HttpStatus.OK);
 	}
-	
+
 	@PutMapping("/update")
 	public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		return new ResponseEntity<String>("Updated Employee Payroll Data for: " + employeePayrollDTO, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("/delete/{empId}")
 	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
 		return new ResponseEntity<String>("Delete Call Success for id: " + empId, HttpStatus.OK);
