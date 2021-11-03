75f235d9ed2dca8b59547d124c229d0c4906648c
Swathi <swathi.h@perfios.com>
2021-11-03 19:31:36 +0530

    MODIFIED: Logging to console based on different environment

--- src/main/java/com/brigdelabz/springpayrollapp/SpringPayrollAppApplication.java
+++ src/main/java/com/brigdelabz/springpayrollapp/SpringPayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.brigdelabz.springpayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringPayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringPayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		ApplicationContext context = SpringApplication.run(SpringPayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
