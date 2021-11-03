6ceee81f015c2425b95bf05414413e4f45218ba0
Swathi <swathi.h@perfios.com>
2021-11-03 20:29:10 +0530

    ADDED: Validations to name and salary

--- src/main/java/com/brigdelabz/springpayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/brigdelabz/springpayrollapp/controller/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.brigdelabz.springpayrollapp.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class EmployeePayrollController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData employeePayrollData = null;
 		employeePayrollData = employeePayrollService.createEmployeePayrollData(employeePayrollDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Created Employee Payroll Data Successful", employeePayrollData);
@@ -52,7 +54,7 @@ public class EmployeePayrollController {
 
 	@PutMapping("/update/{empId}")
 	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,
-			@RequestBody EmployeePayrollDTO employeePayrollDTO) {
+			@Valid @RequestBody EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData employeePayrollData = null;
 		employeePayrollData = employeePayrollService.updateEmployeePayrollData(empId, employeePayrollDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Updated Employee Payroll Data Successful", employeePayrollData);
--- src/main/java/com/brigdelabz/springpayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/brigdelabz/springpayrollapp/dto/EmployeePayrollDTO.java
@@ -1,8 +1,14 @@
 package com.brigdelabz.springpayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
 
+	@Pattern(regexp = "^[A-Z]{1}[a-zA_Z\\s]{2,}$", message = "Employee name Invalid")
 	public String name;
+	
+	@Min(value = 500, message = "Minimum wage should be more than 500")
 	public long salary;
 
 	public EmployeePayrollDTO(String name, long salary) {
