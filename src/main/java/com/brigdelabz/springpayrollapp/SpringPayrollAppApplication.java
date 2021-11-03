53492a21c39f3f5b2b942696ae9da56fe16327b5
Swathi <swathi.h@perfios.com>
2021-11-03 19:04:59 +0530

    ADDED: Lombok library for logging

--- src/main/java/com/brigdelabz/springpayrollapp/SpringPayrollAppApplication.java
+++ src/main/java/com/brigdelabz/springpayrollapp/SpringPayrollAppApplication.java
@@ -3,11 +3,15 @@ package com.brigdelabz.springpayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringPayrollAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started");
 	}
 
 }
