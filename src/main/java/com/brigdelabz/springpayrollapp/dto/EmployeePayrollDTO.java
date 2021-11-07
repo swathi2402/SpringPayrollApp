0cf604ae34d002535f07ad04312b7a365c94e4ba
Swathi <swathi.h@perfios.com>
2021-11-07 16:36:02 +0530

    ADDED: Validations to every field

--- src/main/java/com/brigdelabz/springpayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/brigdelabz/springpayrollapp/dto/EmployeePayrollDTO.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -17,11 +20,21 @@ public @ToString class EmployeePayrollDTO {
 
 	@Min(value = 500, message = "Minimum wage should be more than 500")
 	public long salary;
+	
+	@Pattern(regexp="male|female",message="Gender needs to be male or female")
 	public String gender;
 	
 	@JsonFormat(pattern="dd MMM yyyy")
+	@NotNull(message="Start date should not be empty")
+	@PastOrPresent(message = "Start date should be past or todays date")
 	public LocalDate startDate;
+	
+	@NotBlank(message = "Note cannot be empty")
 	public String note;
+	
+	@NotBlank(message = "ProfilePic cannot be empty")
 	public String profilePic;
+	
+	@NotNull(message= "department should not be empty")
 	public List<String> department;
 }
