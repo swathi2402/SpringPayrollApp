e33f449afca32aee3c20e9ce6c004ca468e42ee8
Swathi <swathi.h@perfios.com>
2021-11-08 17:23:13 +0530

    ADDED: Native query added to get employee by department

--- src/main/java/com/brigdelabz/springpayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/brigdelabz/springpayrollapp/repository/EmployeePayrollRepository.java
@@ -1,9 +1,14 @@
 package com.brigdelabz.springpayrollapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.brigdelabz.springpayrollapp.model.EmployeePayrollData;
 
-public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer>{
+public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
 
+	@Query(value = "SELECT * FROM employee_payroll, employee_department WHERE employee_id = id and department = :department", nativeQuery = true)
+	List<EmployeePayrollData> findEmployeeByDepartment(String department);
 }
