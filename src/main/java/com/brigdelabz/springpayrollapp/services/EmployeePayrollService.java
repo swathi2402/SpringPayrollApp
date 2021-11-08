3a8f2baea09fb34b9c93427aca7805d105a05bbc
Swathi <swathi.h@perfios.com>
2021-11-08 16:58:37 +0530

    MODIFIED: Saving the employee details in database instead of list

--- src/main/java/com/brigdelabz/springpayrollapp/services/EmployeePayrollService.java
+++ src/main/java/com/brigdelabz/springpayrollapp/services/EmployeePayrollService.java
@@ -3,15 +3,23 @@ package com.brigdelabz.springpayrollapp.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brigdelabz.springpayrollapp.dto.EmployeePayrollDTO;
 import com.brigdelabz.springpayrollapp.exceptions.EmployeePayrollException;
 import com.brigdelabz.springpayrollapp.model.EmployeePayrollData;
+import com.brigdelabz.springpayrollapp.repository.EmployeePayrollRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class EmployeePayrollService implements IEmployeePayrollService {
 
+	@Autowired
+	private EmployeePayrollRepository employeeRepository;
+	
 	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 
 	@Override
@@ -28,9 +36,10 @@ public class EmployeePayrollService implements IEmployeePayrollService {
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData employeePayrollData = null;
-		employeePayrollData = new EmployeePayrollData(employeePayrollList.size() + 1, employeePayrollDTO);
+		employeePayrollData = new EmployeePayrollData(employeePayrollDTO);
 		employeePayrollList.add(employeePayrollData);
-		return employeePayrollData;
+		log.debug("Employee Data: "+ employeePayrollData.toString());
+		return employeeRepository.save(employeePayrollData);
 	}
 
 	@Override
